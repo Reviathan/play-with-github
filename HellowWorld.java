817c56a579079d68aec22b3a62387a275138c655
Reviathan <wwflud@gmail.com>
2013-09-13 12:41:48 +0900

    Update HellowWorld.java

--- HellowWorld.java
+++ HellowWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld{
     public static void main(String[] args){
         System.out.printfIn("Hello GitHub second!!");
         System.out.printfIn("Hello GitHub second!!");        
+        System.out.printfIn("Hello GitHub second!!");
+        System.out.printfIn("Hello GitHub second!!");          
       }
 }
