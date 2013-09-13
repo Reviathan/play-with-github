39169b7d04aaf101625538038ddd2d349477586c
Reviathan <wwflud@gmail.com>
2013-09-13 12:40:54 +0900

    Update HellowWorld.java
    コード追加

--- HellowWorld.java
+++ HellowWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.printfIn("Hello GitHub second!!");
+        System.out.printfIn("Hello GitHub second!!");        
       }
 }
